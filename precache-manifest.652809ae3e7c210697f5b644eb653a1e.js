0e7a6a5e92a3999c67b7b8dd7be06b757cb6c722
Milos <milosrancic94@gmail.com>
2019-03-20 16:56:43 +0100

    Updates

---
+++ precache-manifest.652809ae3e7c210697f5b644eb653a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "795d8df073f37e88dcc1",
+    "url": "/reactjs-pomodoro-clock/static/css/main.6ed7f461.chunk.css"
+  },
+  {
+    "revision": "795d8df073f37e88dcc1",
+    "url": "/reactjs-pomodoro-clock/static/js/main.eb3e05f4.chunk.js"
+  },
+  {
+    "revision": "5223ec21cc336827b6fd",
+    "url": "/reactjs-pomodoro-clock/static/js/runtime~main.bfcd5d13.js"
+  },
+  {
+    "revision": "b8f8350390dfe16b0c8d",
+    "url": "/reactjs-pomodoro-clock/static/css/2.2811451d.chunk.css"
+  },
+  {
+    "revision": "b8f8350390dfe16b0c8d",
+    "url": "/reactjs-pomodoro-clock/static/js/2.5210a3d0.chunk.js"
+  },
+  {
+    "revision": "a5633b781c0d5f56fc74d34053d043aa",
+    "url": "/reactjs-pomodoro-clock/index.html"
+  }
+];
